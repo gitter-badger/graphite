992c8b51d89934c15d0e1b8b4c8fcdb735c0cd82
Scott M. Likens <scott@likens.us>
2012-12-18 18:17:48 -0800

    swap out readme

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 maintainer       "Scott M. Likens"
 maintainer_email "scott@likens.us"
-license          "Free as in beer"
+license          "Apache 2.0"
 description      "Installs/Configures graphite"
 
-version          "0.0.1"
-depends "python"
-depends "runit"
+version          "0.0.2"
+%w{python carbon}.each do |dp|
+  depends dp
+end
