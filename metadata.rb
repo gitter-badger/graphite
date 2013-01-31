3ab71aa3cf0cb7cf2fe24f4a17725047adf43e5d
Scott M. Likens <scott@likens.us>
2013-01-30 17:35:18 -0800

    bump to 0.0.5

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "scott@likens.us"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 
-version          "0.0.4"
+version          "0.0.5"
 %w{python carbon git}.each do |dp|
   depends dp
 end
