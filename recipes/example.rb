9ba2056e563925a30a93b1150bb5e9eca8df156a
Scott M. Likens <scott@likens.us>
2013-05-11 02:52:18 +0000

    add example as a test scenario and add test units

--- recipes/example.rb
+++ recipes/example.rb
@@ -13,22 +13,18 @@ end
 carbon_cache "carbon-cache-a" do
   action :create
   storage_aggregation({:min => { :pattern => "\.min$", :xfilesfactor => "0.1", :aggregationmethod => "min" }, :max => { :pattern => "\.max$", :xfilesfactor => "0.1", :aggregationmethod => "max" }, :sum => { :pattern => "\.count$", :xfilesfactor => "0", :aggregationmethod => "sum" }, :default_average => { :pattern => ".*", :xfilesfactor => "0.5", :aggregationmethod => "average"}})
-end
-
-carbon_cache "carbon-cache" do
-  action :start
-  init_style "runit"
   cpu_affinity 1
 end
 
-carbon_relay "relay" do
-  action :create
-end
-
 carbon_relay "carbon-relay" do
-  action :start
+  action :create
   init_style "runit"
   cpu_affinity 0
+end
+
+
+graphite_web "graphite-web-stable" do
+  action :git
 end
 
 cookbook_file "/opt/graphite/conf/graphTemplates.conf" do
@@ -37,22 +33,14 @@ cookbook_file "/opt/graphite/conf/graphTemplates.conf" do
   group "graphite"
 end
 
-graphite_web "graphite-web-stable" do
-  action :git
-end
-
-graphite_web "graphite-ui" do
-  action :create
-end
-
 graphite_web "graphite-web" do
-  action :start
+  action :create
   init_style "runit"
   workers "2"
   backlog 65535
   listen_port 8080
   listen_address "0.0.0.0"
-  cpu_affinity 1
+  cpu_affinity "3-4"
   user "graphite"
   group "graphite"
   graphite_home "/opt/graphite"
